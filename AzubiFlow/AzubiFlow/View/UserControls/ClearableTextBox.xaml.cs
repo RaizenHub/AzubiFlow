b09e0bbb5dd2dc6294519d121366712caf700cca
Manuel Weiss <manuel.weissblau@gmail.com>
2025-07-27 15:06:19 +0200

    add: usercontrol for textbocks

---
+++ AzubiFlow/AzubiFlow/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AzubiFlow.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl
+    {
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            { 
+                placeholder = value;
+                //implent OnPropertyChanged 
+                txtPlaceholder.Text = placeholder;
+            }
+        }
+
+        private void btnClearTextOnClick(object sender, RoutedEventArgs e)
+        {
+            taskInput.Clear();
+        }
+
+        private void taskInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(taskInput.Text))
+            {
+                txtPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                txtPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
--- AzubiFlow/AzubiFlow/obj/Debug/net8.0-windows/App.g.cs
+++ AzubiFlow/AzubiFlow/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace AzubiFlow {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace AzubiFlow {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.7.0")]
         public static void Main() {
             AzubiFlow.App app = new AzubiFlow.App();
             app.InitializeComponent();
